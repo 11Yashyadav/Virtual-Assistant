8b87b6c876349c2b4226e4100191af79668ad8d7
Yash Yadav <yashyadav9325@gmail.com>
2025-05-13 20:39:47 +0530

    Delay to ensure greeting completes before starting recognition

--- script.js
+++ script.js
@@ -29,6 +29,10 @@ btn.addEventListener("click", function () {
     } else if (command.includes("open whatsapp")) {
       speak("Opening WhatsApp...");
       window.open("https://www.whatsapp.com", "_blank");
+    } else if (command.includes("stop")) {
+      speak(
+        "Okay, you can start me anytime by clicking Starting Listening button."
+      );
     } else {
       // Perform a Google search if command not recognized
       speak("Searching Google for " + command);
@@ -41,6 +45,13 @@ btn.addEventListener("click", function () {
 
   // Greet the user and then start listening
   speak("Hello I'm TalkieTech build by Yash Yadav, how can I help you?");
+
+  // Delay to ensure greeting completes before starting recognition
+  setTimeout(() => {
+    btn.innerHTML = "Listening...👂";
+    btn.classList.add("listening");
+    recognition.start();
+  }, 4500);
 
   // When a result is received
   recognition.onresult = (event) => {
